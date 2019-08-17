3e2cdfe7c5ac90f540281eec9196e1bb97181f99
esmyd <gregorioenrique14@gmail.com>
2019-08-17 13:21:42 -0500

    live

--- public/js/indicador2.js
+++ public/js/indicador2.js
@@ -1060,7 +1060,17 @@ $('#indicador').show();
   
    
 
+    var tablaDatos = $("#live");
+    var route = "http://www.auditoria.damplus.net/live";
+    $.get(route, function(res){
+        $(res).each(function(key,value){
+            
+            tablaDatos.append("<tr><td>"+value.user_group+"</td><td>"+value.campaign_id+"</td><td>"+ value.cantidad +"</td></tr>");
 
+           
+        });
+        
+    });
   
 
 
